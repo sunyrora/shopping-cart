c1bb6925cd503158c3f67a921bee8d1d3193a5f7
sunyrora <sunyrora@gmail.com>
2022-01-05 13:41:10 +0100

    CartScreen style

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,11 +1,30 @@
 import "./CartScreen.css";
+import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
     return (
         <div className="cartscreen">
-            Cart Screen
+            <div className="cartscreen__left">
+                <h2>Shopping Cart</h2>
+                
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cartscreen__right">
+                <div class="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p>$499.99</p>
+                </div>
+                <div><button>Proceed to Checkout</button></div>
+            </div>
         </div>
     )
 };
 
 export default CartScreen;
+
