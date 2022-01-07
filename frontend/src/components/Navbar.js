80e2f81dfe5e7eba2b1a6932f46c44fa15bf9358
sunyrora <sunyrora@gmail.com>
2022-01-07 01:00:13 +0100

    - Show cart icon on the Navbar on mobile view too

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,24 +8,35 @@ const Navbar = ({setSideToggle}) => {
     return (
         <nav className="navbar">
             <div className="navbar__logo">
-                <h2>SJ Shopping cart</h2>
+                <h2><Link to="/">SJ Shopping cart</Link></h2>
             </div>
 
             <ul className="navbar__links">
-                <li>
-                    <Link to="/cart" className="cart__link">
-                        <i className="fas fa-shopping-cart"></i>
-                        <span>
-                            Cart
+                <div className="navbar__links__full">
+                    <li>
+                        <Link to="/cart" className="cart__link">
+                            <i className="fas fa-shopping-cart"></i>
+                            <span>
+                                Cart
+                            </span>
                             <span className="cartlogo__badge">{cart.itemCount}</span>
-                        </span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/">
-                       Shop
-                    </Link>
-                </li>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/">
+                        Shop
+                        </Link>
+                    </li>
+                </div>
+                <div className="navbar__links__mini">
+                    <li>
+                        <Link to="/cart" className="cart__link">
+                            <i className="fas fa-shopping-cart"></i>
+                            <span className="cartlogo__badge">{cart.itemCount}</span>
+                        </Link>
+                    </li>
+                </div>
+
             </ul>
             <div className="hamberger__menu" onClick={setSideToggle}>
                 <div></div>
